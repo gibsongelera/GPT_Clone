bbb344e1e1f111e837cbcfb299c27e614a763557
Gibson Gelera <76902828+gibsongelera@users.noreply.github.com>
2024-04-04 23:51:32 +0800

    Update main.js

--- asset/main.js
+++ asset/main.js
@@ -1,1 +1,9 @@
+const chatInput= document.querySelector('#chat-input');
+const sendButton= document.querySelector('#send-btn');
+let userText = null;
+const handleOutgoingChat = () => {
+    userText =  chatInput.value.trim(); // Get Chat Input Value and Remove extra Spaces
+    console.log(userText);
+}
+sendButton.addEventListener("click",handleOutgoingChat);
 
